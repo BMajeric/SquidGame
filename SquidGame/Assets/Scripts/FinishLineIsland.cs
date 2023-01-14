3c77bf3fc0c1c0c98dc3c47a30583f5211486bd5
Timon <timon.rodic@fer.hr>
2023-01-14 15:34:30 +0100

    finish line i respawn treca igra

---
+++ SquidGame/Assets/Scripts/FinishLineIsland.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLineIsland : MonoBehaviour
+{
+   public class FinishLine : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Invoke("loadaj", 3);
+        
+    }
+    private void loadaj(){
+        SceneManager.LoadScene("StartMenu");
+        /*
+        PlayerMovement a = other.GetComponent<PlayerMovement>();
+        if(a != null){
+           SceneManager.LoadScene("StartMenu");
+        }
+        */
+    }
+}
+}
--- SquidGame/Assets/Scripts/idiotFellDown.cs
+++ SquidGame/Assets/Scripts/idiotFellDown.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class idiotFellDown : MonoBehaviour
 {
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawn;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
-       
+       player.transform.position = respawn.transform.position;
     }
 }
