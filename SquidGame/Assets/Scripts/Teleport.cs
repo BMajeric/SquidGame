849990b28162d09374abd8ed6d614544404e1f74
BMajeric <borna.majeric@gmail.com>
2023-01-16 20:35:10 +0100

    Added highlighting the next tile for GT game

--- SquidGame/Assets/Scripts/Teleport.cs
+++ SquidGame/Assets/Scripts/Teleport.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Teleport : MonoBehaviour
 {
+    // Creating the positions field that holds the teleporatation positions for each tile
+    private Transform[] positions = new Transform[10];
+
     public Transform pos1;
     public Transform pos2;
     public Transform pos3;
@@ -18,26 +21,45 @@ public class Teleport : MonoBehaviour
     public Transform finish;
     public GameObject player;
 
-    private Transform[] positions = new Transform[10];
-   
+    // Creating the tiles field that holds the tile game objects (used for the highlighting option)
+    private GameObject[] tiles = new GameObject[10];
 
+    [SerializeField] private GameObject left1;
+    [SerializeField] private GameObject right1;
+    [SerializeField] private GameObject left2;
+    [SerializeField] private GameObject right2;
+    [SerializeField] private GameObject left3;
+    [SerializeField] private GameObject right3;
+    [SerializeField] private GameObject left4;
+    [SerializeField] private GameObject right4;
+    [SerializeField] private GameObject left5;
+    [SerializeField] private GameObject right5;
+
+    // Materials for highlighted and non highlighted tiles
+    [SerializeField] private Material BaseMaterial;
+    [SerializeField] private Material HighlightMaterial;
+
+    // Variable used for determinining if lives were lost (the player died)
     int old_lives = Globals.lives;
 
-    private int step = 0; 
-    private bool teleported = false;
-    private bool first = true;
-    private Vector3 distance1;
-    private Vector3 distance2;
-    private Vector3 distance3;
-    private Vector3 distance4;
-    private bool left = false;
+    // Determines the row of glass tiles, used for iteration through fields
+    private int step = 0;
 
+    // Which tile is highlighted:
+    //      0 -> left
+    //      1 -> right
+    private int side = 0;
+
+    // Delay imput variables
     private float time = 0f;
     private float timeDelay = 2f;
+    private float chooseTime = 0f;
+    private float chooseTimeDelay = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize positions field
         positions[0] = pos1;
         positions[1] = pos2;
         positions[2] = pos3;
@@ -48,114 +70,98 @@ public class Teleport : MonoBehaviour
         positions[7] = pos8;
         positions[8] = pos9;
         positions[9] = pos10;
-        /*
-        distance1 = pos3.transform.position - pos1.transform.position;
-        distance2 = pos4.transform.position - pos1.transform.position;
-        distance3 = pos4.transform.position - pos2.transform.position;
-        distance4 = pos3.transform.position - pos2.transform.position;
-        */
+
+        // Initialize tiles field
+        tiles[0] = left1;
+        tiles[1] = right1;
+        tiles[2] = left2;
+        tiles[3] = right2;
+        tiles[4] = left3;
+        tiles[5] = right3;
+        tiles[6] = left4;
+        tiles[7] = right4;
+        tiles[8] = left5;
+        tiles[9] = right5;
     }
 
     // Update is called once per frame
     void Update()
     {
         time = time + 1f * Time.deltaTime;
-        if (time >= timeDelay)
+        chooseTime = chooseTime + 1f * Time.deltaTime;
+        if (chooseTime>= chooseTimeDelay && step < 10)
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                /*
-                left = true;
-                if (first)
-                {
-                    player.transform.position = pos1.transform.position;
-                    first = false;
-                }
-                else
-                {
-                    if (left)
-                    {
-                        pos1.transform.position += distance1;
-                        pos2.transform.position += distance1;
-                        player.transform.position = pos1.transform.position;
-                        Debug.Log("Na lijevoj idem lijevo");
-                    }
-                    else
-                    {
-                        pos1.transform.position += distance4;
-                        pos2.transform.position += distance4;
-                        player.transform.position = pos1.transform.position;
-                        Debug.Log("Na desnoj idem lijevo");
-                    }
-                }
-                */
+                side = 0;
+                chooseTime = 0f;
+                Debug.Log(side);
+
+                // Hihlight the next left tile
+                tiles[step].GetComponent<MeshRenderer>().material = HighlightMaterial;
+                tiles[step+1].GetComponent<MeshRenderer>().material = BaseMaterial;
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                side = 1;
+                chooseTime = 0f;
+                Debug.Log(side);
+
+                // Hihlight the next right tile
+                tiles[step + 1].GetComponent<MeshRenderer>().material = HighlightMaterial;
+                tiles[step].GetComponent<MeshRenderer>().material = BaseMaterial;
+            }
+
+        }
+
+        if (time >= timeDelay && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (side == 0)
+            {
                 if(step >= 10 ){
                     player.transform.position = finish.transform.position;
                     Invoke("loadaj",3 );
                 }else{
                     player.transform.position = positions[step].transform.position;
+                    tiles[step].GetComponent<MeshRenderer>().material = BaseMaterial;
                     step += 2;
                     time = 0f;
                     Debug.Log(step);
                     Debug.Log(player.transform.position);
                 }
-            
-
             }
-
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            else if (side == 1)
             {
-               /*
-                left = false;
-                if (first)
-                {
-                    player.transform.position = pos2.transform.position;
-                    first = false;
-                }
-                else
-                {
-                    if (!left)
-                    {
-                        pos2.transform.position += distance3;
-                        pos1.transform.position += distance3;
-                        player.transform.position = pos2.transform.position;
-
-                        Debug.Log("Na desnoj idem desno");
-                    }
-                    else
-                    {
-                        pos2.transform.position += distance2;
-                        pos1.transform.position += distance2;
-                        player.transform.position = pos2.transform.position;
-                        Debug.Log("Na lijevoj idem desno");
-                    }
-                }
-                */
-                 if(step >= 10 ){
+                if(step >= 10 ){
                     player.transform.position = finish.transform.position;
                     Invoke("loadaj",3 );
                 }else{
                     player.transform.position = positions[step+1].transform.position;
+                    tiles[step + 1].GetComponent<MeshRenderer>().material = BaseMaterial;
                     step += 2;
                     time = 0f;
                     Debug.Log(step);
                     Debug.Log(player.transform.position);
                 }
-            
             }
+
 
         }
 
 
         if(old_lives != Globals.lives){
-           step = 0;
-           old_lives = Globals.lives;
+            tiles[step].GetComponent<MeshRenderer>().material = BaseMaterial;
+            tiles[step + 1].GetComponent<MeshRenderer>().material = BaseMaterial;
+            step = 0;
+            old_lives = Globals.lives;
         }
 
     }
 
+
+
     private void loadaj(){
         SceneManager.LoadScene("StartMenu");
     }
-
 }
